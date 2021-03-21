4f054c2f3084cc21343629b4e7acfc4c68671e09
Anton Vodorez <vodorez.anton07@gmail.com>
2021-03-21 16:40:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,109 +1,83 @@
-import { Component } from "react";
-import Section from "./Components/Section";
-import AddSection from "./Components/AddSection";
-import OutputSection from "./Components/OutputSection";
-import Filter from "./Components/Filter";
-import { v4 as uuidv4 } from "uuid";
+import { Component } from 'react';
+import Section from './Components/Section';
+import AddSection from './Components/AddSection';
+import OutputSection from './Components/OutputSection';
+import Filter from './Components/Filter';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-    this.handleFilterUpdate = this.handleFilterUpdate.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+    state = {
+        contacts: [],
+        filter: ''
+      };     
 
-    this.state = {
-      contacts: [],
-      filter: "",
-    };
-  }
-
-  componentDidMount() {
-    const loadedContacts = JSON.parse(localStorage.getItem("contacts"));
-    if (loadedContacts) this.setState({ contacts: loadedContacts });
-  }
-
-  componentDidUpdate(prevState) {
-    const { contacts: currentContacts } = this.state;
-    const { contacts: prevContacts } = prevState;
     if (currentContacts !== prevContacts) {
       localStorage.setItem("contacts", JSON.stringify(currentContacts));
-    }
-  }
-
-  updateContactsList() {
-    let contactsForShow = [];
-    if (this.state.filter.length !== 0) {
-      for (let item of this.state.contacts) {
-        console.log(item);
-        console.log(
-          item.name.toLowerCase() + "  " + this.state.filter.toLowerCase()
-        );
-        if (item.name.toLowerCase().includes(this.state.filter.toLowerCase())) {
-          contactsForShow.push(item);
+      componentDidMount() {
+        const loadedContacts = JSON.parse(localStorage.getItem("contacts"));
+        if (loadedContacts) this.setState({ contacts: loadedContacts });
+      }
+    
+      componentDidUpdate(prevState) {
+        const { contacts: currentContacts } = this.state;
+        const { contacts: prevContacts } = prevState;
+        if (currentContacts !== prevContacts) {
+          localStorage.setItem("contacts", JSON.stringify(currentContacts));
         }
       }
-    } else {
-      contactsForShow = this.state.contacts;
+
+    updateContactsList = () => {
+        const {contacts, filter} = this.state;
+        return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
     }
 
-    return contactsForShow;
-  }
-
-  handleDelete(value) {
-    let id = 0;
-    for (let i = 0; i < this.state.contacts.length; i++) {
-      if (value === this.state.contacts[i].id) {
-        id = i;
-        break;
-      }
+    handleDelete = value => {
+        this.setState(prevState => {
+            return {
+              contacts: prevState.contacts.filter(({id}) => id !== value)
+            }
+          }) 
     }
 
-    this.setState({
-      contacts: this.state.contacts
-        .slice(0, id)
-        .concat(this.state.contacts.slice(id + 1)),
-    });
-  }
-
-  handleFilterUpdate(value) {
-    this.setState({
-      filter: value,
-    });
-    this.updateContactsList();
-  }
-
-  handleButtonClick(nameValue, numberValue) {
-    for (let item of this.state.contacts) {
-      if (item.name === nameValue) {
-        alert(nameValue + " is already in contacts");
-        return;
-      }
+    handleFilterUpdate = value => {
+        this.setState({ 
+            filter: value
+          });
+          this.updateContactsList();   
     }
 
-    this.setState({
-      contacts: this.state.contacts.concat([
-        { id: uuidv4(), name: nameValue, number: numberValue },
-      ]),
-    });
-    this.updateContactsList();
-  }
+    handleButtonClick = (nameValue, numberValue) => {
 
-  render() {
-    return (
-      <div>
-        <Section title={"Phonebook"}>
-          <AddSection onSendData={this.handleButtonClick} />
-        </Section>
-        <h2>Contacts</h2>
-        <Filter onFilterUpdate={this.handleFilterUpdate} />
-        <OutputSection
-          data={this.updateContactsList()}
-          onDelete={this.handleDelete}
-        />
-      </div>
-    );
-  }
+        const contactState = this.state.contacts.find(contact => contact.name.toLowerCase() === nameValue.toLowerCase());
+        contactState &&  alert(nameValue + " is already in contacts");
+        if (!contactState && nameValue && numberValue) {
+            const newContact = {
+                id: uuidv4(),
+                name: nameValue,
+                number: numberValue,
+              };
+              this.setState(prevState => {
+                return {
+                  contacts: [...prevState.contacts, newContact],
+                }
+              });
+            return
+        }
+    };
+
+    render() {
+        return (
+            <div>
+               <Section title={"Phonebook"}>
+                    <AddSection onSendData={this.handleButtonClick}/>              
+               </Section>
+                    <h2>Contacts</h2>
+                   <Filter onFilterUpdate={this.handleFilterUpdate}/>
+                   <OutputSection data={this.updateContactsList()} onDelete={this.handleDelete}/>
+            </div>
+        );
+    }
 }
+      
 
 export default App;
